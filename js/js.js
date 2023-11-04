836cdf7d1427dd7128718a3a3e3e0f217bd0359a
Agustin Urrutty <agusurrutty9@gmail.com>
2023-11-04 10:35:29 -0300

    Correcion y finalizacion del JS

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
 
 
-document.addEventListener("DOMContentLoaded", function () {
+function calcular() {
 
     // Precio original por entrada
     const PrecioOriginal = 200;
@@ -8,29 +8,33 @@ document.addEventListener("DOMContentLoaded", function () {
     // Obtener los elementos select
     var cantidadDeEntradas = document.getElementById("Cantidad");
     var descuentoSelect = document.getElementById("selectDescuentos");
+
+
+    // Obtener los valores seleccionados
+    var cantidadEntradas = parseInt(cantidadDeEntradas.value);
+    var descuento = parseInt(descuentoSelect.value);
+
+
+    // Calcular el precio con descuento
+    var precioConDescuento = PrecioOriginal - (PrecioOriginal * (descuento / 100));
+
+    // Calcular el precio total
+    var precioTotal = cantidadEntradas * precioConDescuento;
+
+    // Mostrar el resultado en el div html
+    document.getElementById("resultado").innerText = precioTotal;
+}
+
+function Borrar() {
+    // Obtener los elementos select
     var resultadoDiv = document.getElementById("resultado");
+    var cantidadEntradasSelect = document.getElementById("Cantidad");
+    var descuentoSelect = document.getElementById("selectDescuentos");
 
+    // Limpiar el contenido del div
+    resultadoDiv.textContent = "";
 
-    // Función para calcular el descuento y mostrar el resultado
-    function calcular() {
-
-        // Obtener los valores seleccionados
-        var cantidadEntradas = parseInt(cantidadDeEntradas.value);
-        var descuento = parseInt(descuentoSelect.value);
-
-
-        // Calcular el precio con descuento
-        var precioConDescuento = PrecioOriginal - (PrecioOriginal * (descuento / 100));
-
-        // Calcular el precio total
-        var precioTotal = cantidadEntradas * precioConDescuento;
-
-        // Mostrar el resultado en el div html
-        document.getElementById("resultado").innerText = precioTotal;
-        resultadoDiv.textContent = precioTotal;
-
-    }
-    // Agregar un evento de cambio a los select para llamar a la función calcular
-    cantidadDeEntradas.addEventListener("input", calcular);
-    descuentoSelect.addEventListener("change", calcular);
-})
+    // Restablecer los select
+    cantidadEntradasSelect.value = "";
+    descuentoSelect.selectedIndex = 0;
+}
